60feab526c0bb1c4f76ac7f68ca427fc21152c2a
anousone <touya_ra@hotmail.com>
2017-12-12 12:01:42 +0700

    update password validator

--- TestTopup.js
+++ TestTopup.js
@@ -46,9 +46,9 @@ userValidator
 passValidator
   .is().min(6) // Minimum length 8 
   .is().max(50) // Maximum length 100 
-  .has().uppercase()                              // Must have uppercase letters 
-  .has().lowercase()                              // Must have lowercase letters 
-  .has().digits()                                 // Must have digits 
+  //.has().uppercase()                              // Must have uppercase letters 
+  //.has().lowercase()                              // Must have lowercase letters 
+  //.has().digits()                                 // Must have digits 
   .has().not().spaces() // Should not have spaces 
 //.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values 
 

e5c98c2e8c5f382f42e99420c821c88acd66e0a3
anousone <touya_ra@hotmail.com>
2018-02-19 11:14:08 +0700

    update

--- webpost.js
+++ webpost.js
@@ -452,7 +452,9 @@ var upload = multer({
       cb(null, _current_picture_path);
     },
     filename: function (req, file, cb) {
-      cb(null, file.originalname.replace(path.extname(file.originalname), "") + '-' + makeid(6) + '-' + Date.now() + path.extname(file.originalname));
+      filepath=file.originalname.replace(path.extname(file.originalname), "") + '-' + makeid(6) + '-' + Date.now() + path.extname(file.originalname);
+      console.log(filepath);
+      cb(null, filepath);
     }
   }),
   fileFilter: function (req, file, cb) {
